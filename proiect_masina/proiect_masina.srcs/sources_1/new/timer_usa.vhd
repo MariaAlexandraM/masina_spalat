701212a2a0d3f746f873c55326339dbcbb444351
Maria Moldovan <maria_moldovan_2003@yahoo.com>
2022-05-25 16:02:17 +0300

    Functional project
    Test on Basys 3

---
+++ proiect_masina/proiect_masina.srcs/sources_1/new/timer_usa.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/25/2022 12:57:26 PM
+-- Design Name: 
+-- Module Name: timer_usa - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity timer_usa is
+  Port (CLK_1HZ : in STD_LOGIC;
+  enable : in STD_LOGIC;
+  led_out : out STD_LOGIC);
+end timer_usa;
+
+architecture Behavioral of timer_usa is
+begin	 	
+
+process(CLK_1HZ, enable)
+begin
+if enable = '1' then
+    if falling_edge(CLK_1HZ) then 
+        led_out <= '0';
+     end if;
+else
+    if falling_edge(CLK_1HZ) then 
+        led_out <= '1';
+     end if;
+end if;
+end process;
+
+end Behavioral;
